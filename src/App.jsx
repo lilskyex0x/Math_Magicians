d41178b3d50bf142aec567054935e988448d9c81
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-09-19 21:29:18 +0630

    Add react router inside main App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import './style.css';
+import {
+  Route, Routes, Outlet, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
 import DisplayQuote from './components/Quotes';
+import Home from './components/Home';
 
-function App() {
+export default function App() {
   return (
     <>
-      <DisplayQuote />
-      <Calculator />
+      <div className="wrapper">
+        <h1>Math Magician</h1>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route path="quotes" element={<DisplayQuote />} />
+          </Route>
+        </Routes>
+      </div>
     </>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div className="layout">
+      <nav className="navbar">
+        <ul className="navItems">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
